2450f7c831043ddd512258c0a15ceeb73203626a
TobiasB91 <tobbra91@gmail.com>
2022-02-08 14:29:02 +0100

    ...

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const TELEGRAM_TOKEN = "5253701676:AAG4WqkZHp4deWHeivBDiSVbCxRPjP49Ym8";
+const CAT_API_KEY = "d6f22261-825e-47c9-9514-c05ff7e289d2";
+
 const express = require("express");
 const tl = require("express-tl");
 const path = require("path");
@@ -5,12 +8,18 @@ const app = express();
 const sha1 = require("sha1");
 const fs = require("fs");
 const cookieParser = require("cookie-parser");
+const https = require("https");
+const TeleBot = require("telebot");
+const bot = new TeleBot(TELEGRAM_TOKEN);
+const FormData = require("form-data");
+const { application } = require("express");
 
 app.engine("tl", tl);
 app.set("views", "./views");
 app.set("view engine", "tl");
 app.use(express.static("static"));
 app.use(cookieParser());
+
 
 app.get("/check/:stage", (req, res) => {
   let codeHash = req.query.code;
@@ -105,5 +114,75 @@ app.get("/:stage", (req, res) => {
     res.render("stage", JSON.parse(data));
   });
 });
+
+
+
+const BOT_TEXT_PHRASES = [
+  "Es gibt einen Weg mich zu überzeugen, Dir das Lösungswort zu verraten. Leicht einzuschüchtern bin ich aber nicht! 💪🐭",
+  "🐭🐭🐭🐭🐭",
+  "Ich mag Käse.",
+  "Piep Piep 🐭", 
+  "Leicht einzuschüchtern bin ich nicht! 💪🐭"
+];
+
+bot.on(["text", "audio", "voice"], msg => {
+  let j = Math.floor(Math.random() * BOT_TEXT_PHRASES.length);
+  return msg.reply.text(BOT_TEXT_PHRASES[j]);
+});
+
+bot.on("photo", msg => {
+  bot.getFile(msg.photo[msg.photo.length-1].file_id).then(file => {
+    var stream = fs.createWriteStream(file.file_path);
+    https.get(`https://api.telegram.org/file/bot${TELEGRAM_TOKEN}/${file.file_path}`, res => {
+      res.pipe(stream);
+      stream.on("finish", () => {
+        stream.close(() => {
+          let options = {
+            host: "api.thecatapi.com", 
+            protocol: "https:",
+            path: "/v1/images/upload", 
+            headers: { 
+              "content-type": "multipart/form-data;", 
+              "x-api-key": CAT_API_KEY
+            }
+          };
+
+          let form = new FormData();
+          form.append("file", fs.createReadStream(file.file_path));
+          form.append("sub_id", "");
+          let req2 = form.submit(options, (err, res2) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+
+            let chunks = [];
+
+            res2.on("data", c => {
+              console.log(c);
+              chunks.push(c);
+            });
+
+            res2.on("end", () => {
+              if (!res2.complete) {
+                console.log("ERR");
+              }
+              console.log(Buffer.concat(chunks).toString());
+              console.log("end");
+            });
+            
+          }); 
+
+        });
+      });
+    }).on("error", err => {
+      console.log(err);
+    });
+  }).catch(err => {
+    console.log(err);
+  });
+});
+
+bot.start();
 
 const server = app.listen(2030);
